c2cbca88d441e7303b5cdc94757e8a41cea488b5
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2018-05-16 17:45:00 +0100

    RL on randomly generated Transport problems.

--- learning/rl_driver.py
+++ learning/rl_driver.py
@@ -2,19 +2,38 @@
 
 import subprocess
 import sys
+import time
 
 conf = 'learning(ff,learning_rate=%f)'
 learning_rate = 0.1
+transport_generator = '../../../IPC/own-transport/generator14L/city-generator.py'
 
-domain = sys.argv[1]
-problem = sys.argv[2]
-iterations = int(sys.argv[3])
+if len(sys.argv) == 4:
+    domain = sys.argv[1]
+    problem = sys.argv[2]
+    iterations = int(sys.argv[3])
+    generate = False
+else:
+    iterations = int(sys.argv[1])
+    problem = 'problem.pddl'
+    domain = '../../../IPC/own-transport/domain.pddl'
+    generate = True
+
+
+def generate_transport_problem():
+    ipc_generator_command = ['python', transport_generator, '15', '1000', '3', '100', '3', '6', str(time.time())]
+    problem = subprocess.check_output(ipc_generator_command).decode('utf-8')
+    problem_file = open('problem.pddl', 'w')
+    problem_file.write(problem)
+    problem_file.close()
 
 
 log = open('rl_driver_log.txt', 'w')
 
 for i in range(1,iterations+1):
     print(i)
+    if generate:
+        generate_transport_problem()
     subprocess.call(['../fast-downward.py', '--build', 'release64',  domain,  problem, '--search',  conf % learning_rate])
     weights = open('weights.txt')
     log.write(weights.read())
@@ -25,3 +44,4 @@ for i in range(1,iterations+1):
         learning_rate /= 2
 
 log.close()
+
