d8aba7682731776b8f432a2ff62e431f0e68db53
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2018-07-20 15:53:44 +0100

    Single-subroutine reference solutions and multiple runs per problem.

--- learning/rl_driver.py
+++ learning/rl_driver.py
@@ -22,8 +22,13 @@ from parking_generator import ParkingGenerator
 heuristic = 'h1=ff(transform=adapt_costs(one))'
 search = 'learning(h1,t=%d)'
 ref_search1 = 'eager_greedy(h1)'
-ref_search2 = 'learning(h1,weights=uniform.txt)' # it's ok if uniform.txt doesn't exist
-ref_search_list = [ref_search1, ref_search2]
+ref_search2 = 'learning(h1,weights=weights2of6.txt)'
+ref_search3 = 'learning(h1,weights=weights3of6.txt)'
+ref_search4 = 'learning(h1,weights=weights4of6.txt)'
+ref_search5 = 'learning(h1,weights=weights5of6.txt)'
+ref_search6 = 'learning(h1,weights=weights6of6.txt)'
+ref_search_list = [ref_search1, ref_search2, ref_search3,
+    ref_search4, ref_search5, ref_search6]
 
 learning_rate = 1.0
 target_problem_time = 0.3
@@ -40,6 +45,7 @@ N_CURBS = 9
 N_CARS = 16
 
 N_SAMPLES = 10
+RUNS_PER_PROBLEM = 20
 
 generator = TransportGenerator(N_TRUCKS, N_PACKAGES)
 #generator = ParkingGenerator(N_CURBS, N_CARS)
@@ -107,57 +113,62 @@ def n_states():
         r *= s
     return r
 
+def compute_reference_costs(problem_path):
+    costs = []
+    for ref_search in ref_search_list:
+        try:
+            reference_output = get_output_with_timeout(['../fast-downward.py',
+                '--build', 'release64',  domain,  problem_path,
+                '--heuristic', heuristic, '--search',  ref_search])
+            cost = get_cost(reference_output)
+            print('Reference cost:', cost)
+            costs.append(cost)
+        except subprocess.TimeoutExpired:
+            print('Failed to find a reference solution.')
+    return costs
+
 class NoRewardException(Exception):
     pass
 
-def compute_reward(problem_time, plan_cost):
-
-    # Compute the reference solutions
-    if generate or not hasattr(compute_reward, 'ref_cost'):
-        compute_reward.ref_cost = -1
-        ref_costs = []
-        for ref_search in ref_search_list:
-            try:
-                reference_output = get_output_with_timeout(['../fast-downward.py',
-                    '--build', 'release64',  domain,  problem_path, # should be passed as arg
-                    '--heuristic', heuristic, '--search',  ref_search])
-                #compute_reward.ref_cost = get_cost(reference_output)
-                ref_costs.append(get_cost(reference_output))
-            except subprocess.TimeoutExpired:
-                print('Failed to find a reference solution.')
-                if not generate:
-                    sys.exit('Terminating.')
-        print('Reference costs:', ref_costs)
-        if len(ref_costs) > 0:
-            compute_reward.ref_cost = min(ref_costs)
-    
+def compute_ipc_reward(plan_cost, reference_costs):
     # If no reference solution
-    if compute_reward.ref_cost < 0:
-        if plan_cost >= 0:
-            return 2.0 # fixed reward for solving a problem without reference solution
+    if len(reference_costs) == 0:
+        if plan_cost > 0:
+            return 2.0
         raise NoRewardException()
     # Otherwise
     if plan_cost < 0:
         return 0.0
-    return compute_reward.ref_cost / plan_cost
+    elif plan_cost == 0:
+        raise NoRewardException()
+    else:
+        return min(reference_costs) / plan_cost
 
-def gradient_update(params, history, avg_reward):
+
+
+
+def gradient_update(params, action_count, reward):
     
+    update = np.zeros(params.shape)
+    for state_params, actions, update_row in zip(params, action_count, update):
+        pi = softmax(state_params)
+        state_params0 = np.copy(state_params)
+        for i in range(N_ACTIONS):
+            gradient = np.array([0.0] * N_ACTIONS)
+            for j in range(N_ACTIONS):
+                if i == j:
+                    gradient[j] = pi[i] * (1 - pi[j])
+                else:
+                    gradient[j] = pi[i] * (- pi[j])
+            update_row += learning_rate * reward * actions[i] * gradient
+    return update
+
+def replay_update(params, history, avg_reward):
     update = np.zeros(params.shape)
     for s in range(N_SAMPLES):
         (action_count, reward) = random.choice(history) #history[-1]
         centered_reward = reward - avg_reward
-        for state_params, actions, update_row in zip(params, action_count, update):
-            pi = softmax(state_params)
-            state_params0 = np.copy(state_params)
-            for i in range(N_ACTIONS):
-                gradient = np.array([0.0] * N_ACTIONS)
-                for j in range(N_ACTIONS):
-                    if i == j:
-                        gradient[j] = pi[i] * (1 - pi[j])
-                    else:
-                        gradient[j] = pi[i] * (- pi[j])
-                update_row += learning_rate * centered_reward * actions[i] * gradient
+        update += gradient_update(params, action_count, centered_reward)
     update /= N_SAMPLES
     return update
 
@@ -180,6 +191,7 @@ else:
 
 n_iter = 0
 avg_reward = 0.0
+avg_problem_reward = 0.0
 history = []
 
 search_time = int(1000 * 10 * target_problem_time - preprocessing_time)
@@ -190,65 +202,75 @@ start_time = time.time()
 while time.time() - start_time < training_time:
     
     problem_path = get_problem()    
+    reference_costs = compute_reference_costs(problem_path)
+    update = np.zeros(params.shape)
     
-    problem_time = 0.0
-    plan_cost = -1
-    try:
-        problem_start = time.time()
-        planner_output = get_output_with_timeout(['../fast-downward.py',
-            '--build', 'release64',  domain,  problem_path,
-            '--heuristic', heuristic, '--search',  search % search_time])
-        problem_time = time.time() - problem_start
-        print('Problem time: ', problem_time)
-        #reward = 10*target_problem_time - problem_time
-        plan_cost = get_cost(planner_output)
-    except subprocess.TimeoutExpired:
-        print('Failed to find a solution.')
+    if n_iter > 0:
+        avg_reward += (1 / n_iter) * (avg_problem_reward - avg_reward)
     
-    if plan_cost == 0:
-        continue
+    for i in range(RUNS_PER_PROBLEM):
+        update = np.zeros(params.shape)
+        problem_time = 0.0
+        plan_cost = -1
+        try:
+            problem_start = time.time()
+            planner_output = get_output_with_timeout(['../fast-downward.py',
+                '--build', 'release64',  domain,  problem_path,
+                '--heuristic', heuristic, '--search',  search % search_time])
+            problem_time = time.time() - problem_start
+            print('Problem time: ', problem_time)
+            #reward = 10*target_problem_time - problem_time
+            plan_cost = get_cost(planner_output)
+        except subprocess.TimeoutExpired:
+            print('Failed to find a solution.')
+        
+        if plan_cost == 0:
+            continue
+        
+        print ('Plan cost:', plan_cost)
+        try:
+            reward = compute_ipc_reward(plan_cost, reference_costs)
+        except NoRewardException:
+            continue
+        
+        reward_log.write(str(reward))
+        reward_log.write('\n')
+        reward_log.flush()
+        avg_problem_reward += reward
+        
+        action_count = np.zeros(STATE_SPACE+(N_ACTIONS,))
+        trace_file = open('trace.txt')
+        lines = trace_file.readlines()
+        for l in lines:
+            numbers = [ int(x) for x in l.split()]
+            action_count[tuple(numbers)] += 1
+        debug_log.write(str(action_count.sum()))
+        debug_log.write(' ')
+        action_sum = action_count.sum()
+        if action_sum < 0.5:
+            print('The trace is empty.')
+            debug_log.write('The trace is empty.\n')
+            continue
+        action_count = action_count / action_sum
+        
+        flat_action_count = action_count.reshape(-1, N_ACTIONS)
+        c_reward = reward - avg_reward
+        print('Reward: ', reward, 'Centered: ', c_reward)
+        print('Action counts:', action_count)
+        #history.append((flat_action_count, reward))
+        
+        #update = gradient_update(params, history, avg_reward)
+        partial_update = gradient_update(params, flat_action_count, c_reward)
+        print('Partial update:')
+        print(partial_update)
+        update += partial_update
+        
     
-    print ('Plan cost:', plan_cost)
-    try:
-        reward = compute_reward(problem_time, plan_cost)
-    except NoRewardException:
-        continue
-    
-    reward_log.write(str(reward))
-    reward_log.write('\n')
-    reward_log.flush()
-    
-    if n_iter == 0:
-        avg_reward = reward
-    else:
-        avg_reward += (1 / n_iter) * (reward - avg_reward)
-    
-    action_count = np.zeros(STATE_SPACE+(N_ACTIONS,))
-    trace_file = open('trace.txt')
-    lines = trace_file.readlines()
-    for l in lines:
-        numbers = [ int(x) for x in l.split()]
-        action_count[tuple(numbers)] += 1
-    debug_log.write(str(action_count.sum()))
-    debug_log.write(' ')
-    action_sum = action_count.sum()
-    if action_sum < 0.5:
-        print('The trace is empty.')
-        debug_log.write('The trace is empty.\n')
-        continue
-    action_count = action_count / action_sum
-    
-    flat_action_count = action_count.reshape(-1, N_ACTIONS)
-    c_reward = reward - avg_reward
-    print('Reward: ', reward, 'Centered: ', c_reward)
-    history.append((flat_action_count, reward))
-    
-    update = gradient_update(params, history, avg_reward)
-    print('Update:\n')
+    print('Complete update:')
     print(update)
-    params += update
+    if n_iter > 0: # skip the first iteration (for the lack of baseline)
+        params += update
 
-    print('Action counts:', action_count)
     print('Weights:')
     print(params)
     save_weights(params)
@@ -263,6 +285,7 @@ while time.time() - start_time < training_time:
     log.write('\n')
     log.flush()
     
+    avg_problem_reward /= RUNS_PER_PROBLEM
     n_iter += 1
 
 
