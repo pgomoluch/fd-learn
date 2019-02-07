c4a4cdf2f164a23d0eb0b095382840411cb1c80d
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2019-02-07 17:44:31 +0000

    Support for aliased configurations.

--- learning/find_cost.py
+++ learning/find_cost.py
@@ -7,17 +7,18 @@ import sys
 from rl_common import get_cost
 
 CONFS = [
-    '--search eager_greedy(ff(transform=adapt_costs(one)))',
-    '--search eager_greedy(goalcount)',
-    '--search astar(ff(transform=adapt_costs(one)))',
-    '--search astar(goalcount)'
+    '--search "eager_greedy(ff(transform=adapt_costs(one)))"',
+    '--search "eager_greedy(goalcount)"',
+    '--search "astar(ff(transform=adapt_costs(one)))"',
+    '--search "astar(goalcount)"',
+    '--alias seq-sat-lama-2011'
 ]
 TIME_LIMIT = 5
 MEMORY_LIMIT = '4G'
 
 
 CONDOR_DIR = 'condor_find_cost'
-CONF_FILE = 'conf.txt'
+CONF_FILE = 'fd.sh'
 CONDOR_SCRIPT_NAME = 'condor.sh'
 CONDOR_SPEC_NAME = 'condor.cmd'
 CONDOR_OUT = 'condor.out'
@@ -27,12 +28,8 @@ CONDOR_LOG = 'condor.log'
 CONDOR_SCRIPT = """#!/bin/bash
 echo starting
 cd {condor_dir}/$1
-pwd
-echo {conf_file}
 cat {conf_file}
-conf=`cat {conf_file}`
-echo /usr/bin/python {fd_path} --build release64 --overall-time-limit {time_limit} --overall-memory-limit {memory_limit} {domain} {problem} $conf
-/usr/bin/python {fd_path} --build release64 --overall-time-limit {time_limit} --overall-memory-limit {memory_limit} {domain} {problem} $conf
+./{conf_file}
 """
 
 CONDOR_SPEC = """universe = vanilla
@@ -45,17 +42,38 @@ requirements = regexp("^sprite[0-9][0-9]", TARGET.Machine)
 queue {n_conf}
 """
 
+FD_PLAIN = "/usr/bin/python {fd_path} --build release64 --overall-time-limit {time_limit} --overall-memory-limit {memory_limit} {domain} {problem} {conf}"
 
-domain = sys.argv[1]
-problem = sys.argv[2]
+FD_ALIAS = "/usr/bin/python {fd_path} --build release64 {conf} --overall-time-limit {time_limit} --overall-memory-limit {memory_limit} {domain} {problem}"
+
+domain = os.path.abspath(sys.argv[1])
+problem = os.path.abspath(sys.argv[2])
 output_file_path = sys.argv[3]
 
 for i, c in enumerate(CONFS):
     subdir = os.path.join(CONDOR_DIR, str(i))
     os.makedirs(subdir, exist_ok=True)
-    c_file = open(os.path.join(subdir, CONF_FILE),'w')
-    c_file.write(c)
+    c_path = os.path.join(subdir, CONF_FILE)
+    c_file = open(c_path, 'w')
+    #c_file.write(c)
+    if '--alias' in c:
+        c_file.write(FD_ALIAS.format(
+            fd_path=os.path.abspath('../fast-downward.py'),
+            conf=c,
+            time_limit=TIME_LIMIT,
+            memory_limit=MEMORY_LIMIT,
+            domain=domain,
+            problem=problem))
+    else:
+        c_file.write(FD_PLAIN.format(
+            fd_path=os.path.abspath('../fast-downward.py'),
+            time_limit=TIME_LIMIT,
+            memory_limit=MEMORY_LIMIT,
+            domain=domain,
+            problem=problem,
+            conf=c))
     c_file.close()
+    os.chmod(c_path, 0o775)
 
 condor_script_path = os.path.join(CONDOR_DIR,CONDOR_SCRIPT_NAME)
 condor_script_file = open(condor_script_path, 'w')
