6870081affe18defdaad7bdea59a8c871bd4828f
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2020-06-18 12:12:59 +0100

    Feature ids for domain-dependent experiments.

--- learning/rsvm/learner.cc
+++ learning/rsvm/learner.cc
@@ -16,6 +16,14 @@ const int FEATURE_IDS[] = {5,8,10,11,12,13,14,15,16,17,18}; // FF DI
 //const int FEATURE_IDS[] = {5,9,19,20,21,22,23,24,25,26,27}; // CEA DI
 //const int FEATURE_IDS[] = {5,8,9,10,11,12,13,14,15,16,17,18}; // FF DI + CEA
 
+//const int FEATURE_IDS[] = {5, 8, 13, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39}; // FF DD Transport / No-mystery
+//const int FEATURE_IDS[] = {5, 8, 13, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47}; // FF DD Parking
+//const int FEATURE_IDS[] = {5, 8, 13, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69}; // FF DD Elevators
+
+//const int FEATURE_IDS[] = {5, 9, 22, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51}; // CEA DD Transport / No-mystery
+//const int FEATURE_IDS[] = {5, 9, 22, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59}; // CEA DD Parking
+//const int FEATURE_IDS[] = {5, 9, 22, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81}; // CEA DD Elevators
+
 const double C = 1.0;
 const std::vector<string> DATA_PATHS = {
     "../data/transport1-10-1000-2-100-2-4-B/",
@@ -27,6 +35,11 @@ const std::vector<string> DATA_PATHS = {
     "../data/park-6-8-B/",
     "../data/elevators-12-3/",
     "../data/no-mystery-like-M22/"
+    //
+    //"../data/14C-parking-bfs-ffu/",
+    //"../data/11C-elevators-bfs-ffu/",
+    //"../data/11C-nomystery-bfs-ffu/"
+    //"../data/11C-floortile-bfs-ffu/"
 };
 const int DATA_LIMITS[] = {7000, 7000, 7000, 5000, 5000, 5000, 5000, 20000, 20000};
 const string MODEL_FILE = "model.txt";
